a9eebe802c96afbd7505d5b7c58121407c9e3233
Ulya-B-Zeng <zengbentuoxmu@outlook.com>
2022-06-14 00:31:30 +0800

    Sorry for mispushing the file.

---
+++ mywork/Work_Inverse_of_Matrices.R
@@ -0,0 +1,45 @@
+## This R script is a Coursera assignment to compute the inverse of cached
+## matrices. 
+
+## makeCacheMatrix: make a function list to catch the inverse and cache it in m.
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function() {m <<- solve(x)}
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## cache Solve. argument ... may be the original matrix
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(...)
+  x$setinverse()
+  m
+}
+## test a matrix
+set.seed(13325)
+mx <- matrix(rnorm(100),10,10)
+cache <- makeCacheMatrix(mx)
+## In this occasion there is not anything in m - (cached information)
+cacheSolve(cache,mx)
+
+## In this occasion there should be a cache in m so the function prints
+## "getting cached data"
+cache_2 <- makeCacheMatrix(mx)
+invisible(cache_2$setinverse())
+cacheSolve(cache_2,mx)
