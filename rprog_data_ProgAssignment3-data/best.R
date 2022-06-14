6ecb9129edfea0fd910f77d80be90972a07ed0c7
Ulya-B-Zeng <zengbentuoxmu@outlook.com>
2022-06-14 15:12:12 +0800

    little changes

--- rprog_data_ProgAssignment3-data/best.R
+++ rprog_data_ProgAssignment3-data/best.R
@@ -1,30 +1,26 @@
 ## best function: to find the best hospital with the least 30-day mortality RATE
 ## for the specified outcome.
 
-best <- function(state=state.abb,outcome=c("heart attack","heart failure","pneumonia")){
+best <- function(state=NULL,outcome=c("heart attack","heart failure","pneumonia")){
   ##0. Read the file
   data_pr <- read.csv("outcome-of-care-measures.csv",header=TRUE,colClasses="character")
   
   ##1. Waring the null:
   if(is.null(state) | is.null(outcome)){
-    stop("Please enter the states and the specified outcomes!")
+    stop("Please enter the state and the specified outcome!")
   }
   
   ##2. Check the validity:
   state_check <- unique(data_pr$State)
   match_state <- match(state, state_check, nomatch=NA)
-  for(i in match_state){
-    if(is.na(i)){
-    stop("invalid state")
-    }
-  }  ## validation of the state, here state can be a vector of several states.
+  if(is.na(match_state)){
+   stop("invalid state")
+  }  ## validation of the state
   
   vec_outcome <- c("heart attack","heart failure","pneumonia")
   match_outc <- match(outcome, vec_outcome,nomatch=NA)
-  for(i in match_outc){
-    if(is.na(i)){
-      stop("invalid outcome")
-    }
+  if(is.na(match_outc)){
+    stop("invalid outcome")
   } ## validation of the outcome, here state can be a vector of several outcomes.
   
   ##3. defining the colnames
@@ -37,8 +33,13 @@ best <- function(state=state.abb,outcome=c("heart attack","heart failure","pneum
     i <- i+1
   } ## trans. to num.
   options(warn = 1)
-  colnames(data[,3:5]) <- c("heart attack","heart failure","pneumonia")
+  names(data)[3:5] <- c("heart attack","heart failure","pneumonia") ## rename
   
-  ##4. split by state
+  ##4. split by state and ordering
   data_split <- split(data,data$State)
+  data_order <- data_split[[state]][order(data_split[[state]][[outcome]],
+                                          data_split[[state]][["Hospital.Name"]],
+                                          decreasing = FALSE,na.last = NA),]
+  result <- head(data_order,1,header=FALSE)
+  print(result[,1])
 }
